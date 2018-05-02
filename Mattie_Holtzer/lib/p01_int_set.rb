94a83d42af0d873abf3589a0fe6c407f4a227ba3
Mattie <mattieholtzer@gmail.com>
2018-05-02 08:45:18 -0700

    finished p01

--- Mattie_Holtzer/lib/p01_int_set.rb
+++ Mattie_Holtzer/lib/p01_int_set.rb
@@ -1,19 +1,34 @@
 class MaxIntSet
+
+  attr_writer :max, :set
+
   def initialize(max)
+    @max = max
+    @set = []
+    @max.times do |time|
+      @set.push(false)
+    end
   end
 
   def insert(num)
+    is_valid?(num)
+    @set[num] = true
   end
 
   def remove(num)
+    is_valid?(num)
+    @set[num] = false
   end
 
   def include?(num)
+    return false if num<1 || num>@max
+    @set[num]
   end
 
   private
 
   def is_valid?(num)
+    raise "Out of bounds" if num<1 || num>@max
   end
 
   def validate!(num)
@@ -27,12 +42,17 @@ class IntSet
   end
 
   def insert(num)
+    @store[num%num_buckets].push(num)
   end
 
   def remove(num)
+    if include?(num)
+      @store[num%num_buckets].delete(num)
+    end
   end
 
   def include?(num)
+    @store[num%num_buckets].include?(num)
   end
 
   private
@@ -55,12 +75,22 @@ class ResizingIntSet
   end
 
   def insert(num)
+    @count+=1
+    if @count>num_buckets
+      resize!
+    end
+    @store[num%num_buckets].push(num)
   end
 
   def remove(num)
+    if include?(num)
+      @count-=1
+      @store[num%num_buckets].delete(num)
+    end
   end
 
   def include?(num)
+    @store[num%num_buckets].include?(num)
   end
 
   private
@@ -74,5 +104,12 @@ class ResizingIntSet
   end
 
   def resize!
+    new_arr = Array.new(num_buckets*2)  { Array.new }
+    @store.each do |arr|
+      arr.each do |num|
+        new_arr[num%(num_buckets*2)].push(num)
+      end
+    end
+    @store = new_arr
   end
 end
