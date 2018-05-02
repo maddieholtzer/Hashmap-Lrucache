0cfd75e0991a52406de3934986a4de4603565ff5
Mattie <mattieholtzer@gmail.com>
2018-05-02 10:16:42 -0700

    finished p02

--- Mattie_Holtzer/lib/p02_hashing.rb
+++ Mattie_Holtzer/lib/p02_hashing.rb
@@ -4,11 +4,21 @@ end
 
 class Array
   def hash
+    ret = ""
+    each do |item|
+      ret+=(item.hash%1000).to_s
+    end
+    ret.to_i
   end
 end
 
 class String
   def hash
+    ret = ""
+    chars.each do |letter|
+      ret+=letter.ord.to_s
+    end
+    ret.to_i
   end
 end
 
@@ -16,6 +26,13 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    ret = 0
+    each do |key, val|
+      temp = ""
+      temp+=(key.hash%1000).to_s
+      temp+=(val.hash%1000).to_s
+      ret+=temp.to_i
+    end
+    ret
   end
 end
